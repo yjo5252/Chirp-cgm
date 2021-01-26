fbdb83de339379c8c313a868983f675161abc480
Ray_home <mingrui@uw.edu>
2021-01-25 23:44:10 -0800

    add activity chart weekly

--- twidere/src/main/kotlin/org/mariotaku/twidere/activity/UsageStatsActivity.kt
+++ twidere/src/main/kotlin/org/mariotaku/twidere/activity/UsageStatsActivity.kt
@@ -19,10 +19,22 @@
 
 package org.mariotaku.twidere.activity
 
+import android.content.res.TypedArray
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.util.TypedValue
 import androidx.annotation.RequiresApi
+import com.github.mikephil.charting.charts.BarLineChartBase
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.XAxis.XAxisPosition
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.components.YAxis.YAxisLabelPosition
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.ValueFormatter
 import kotlinx.android.synthetic.main.activity_usagestats.*
 import org.mariotaku.kpreferences.get
 import org.mariotaku.twidere.R
@@ -79,6 +91,117 @@ class UsageStatsActivity : BaseActivity() {
                 "\uD83D\uDC40 Accounts Followed: ${preferences[followAccountsStats]}"
         userunfollowStatsView.text =
                 "\uD83D\uDD15 Accounts Unfollowed: ${preferences[unfollowAccountsStats]}"
+
+        drawBarChart(weekStats)
     }
 
+    private fun drawBarChart(weekStats: List<Long>) {
+        weekStatsChart.setDrawValueAboveBar(true)
+        weekStatsChart.setPinchZoom(false)
+        weekStatsChart.setDrawGridBackground(false)
+        weekStatsChart.isDoubleTapToZoomEnabled = false
+        weekStatsChart.isHorizontalScrollBarEnabled = false
+        weekStatsChart.isVerticalScrollBarEnabled = false
+        weekStatsChart.isDragEnabled = false
+        weekStatsChart.legend.isEnabled = false
+        weekStatsChart.description.isEnabled = false
+
+        val xAxisFormatter = DayAxisValueFormatter(weekStatsChart)
+        val xAxis: XAxis = weekStatsChart.getXAxis()
+        xAxis.position = XAxisPosition.BOTTOM
+        xAxis.granularity = 1f // only intervals of 1 day
+        xAxis.labelCount = 7
+        xAxis.setDrawGridLines(false)
+        xAxis.valueFormatter = xAxisFormatter
+        xAxis.textSize = 16f
+        xAxis.textColor = fetchAccentColor()
+
+        val yAxisFormatter = MinuteAxisValueFormatter(weekStatsChart)
+        val leftAxis: YAxis = weekStatsChart.axisLeft
+        leftAxis.setLabelCount(4, false)
+        leftAxis.valueFormatter = yAxisFormatter
+        leftAxis.setPosition(YAxisLabelPosition.OUTSIDE_CHART)
+        leftAxis.spaceTop = 15f
+        leftAxis.axisMinimum = 0f // this replaces setStartAtZero(true)
+        leftAxis.textSize = 16f
+        leftAxis.textColor = fetchAccentColor()
+        leftAxis.granularity = 5f
+        leftAxis.isEnabled = false
+
+        weekStatsChart.axisRight.isEnabled = false
+
+        val entries: MutableList<BarEntry> = ArrayList()
+        val colors: ArrayList<Int> = ArrayList()
+        val todayIdx = UseStats.getTodayInWeekIdx()
+
+        for (i in weekStats.indices) {
+            // turn your data into Entry objects
+            entries.add(BarEntry(i.toFloat(),
+                    (weekStats[1]*i / (1000*60) ).toFloat()))
+
+            if (i <= todayIdx)
+                colors.add(Color.parseColor("#16D9DF"))
+            else
+                colors.add(Color.parseColor("#949C9C"))
+        }
+
+        if (weekStatsChart.data != null &&
+                weekStatsChart.data.dataSetCount > 0) {
+            val set = weekStatsChart.data.getDataSetByIndex(0) as BarDataSet
+            set.values = entries
+            set.colors = colors
+            weekStatsChart.data.notifyDataChanged()
+            weekStatsChart.notifyDataSetChanged()
+        } else {
+            val set = BarDataSet(entries, "Values")
+            set.colors = colors
+
+            val data = BarData(set)
+            data.setValueTextColor(fetchAccentColor())
+            data.setValueTextSize(15f)
+            data.barWidth = 0.8f
+            data.setValueFormatter(MinuteAxisValueFormatter(weekStatsChart))
+            weekStatsChart.data = data
+            weekStatsChart.invalidate()
+        }
+    }
+
+    private fun fetchAccentColor(): Int {
+        val typedValue = TypedValue()
+        val a: TypedArray = this.obtainStyledAttributes(typedValue.data,
+                intArrayOf(android.R.attr.textColorPrimary))
+        val color = a.getColor(0, 0)
+        a.recycle()
+        return color
+    }
+
+}
+
+class DayAxisValueFormatter(private val chart: BarLineChartBase<*>) : ValueFormatter() {
+    override fun getFormattedValue(value: Float): String {
+        return when (value){
+            0f -> "Sun"
+            1f -> "Mon"
+            2f -> "Tue"
+            3f -> "Wed"
+            4f -> "Thu"
+            5f -> "Fri"
+            else -> "Sat"
+        }
+    }
+}
+
+class MinuteAxisValueFormatter(private val chart: BarLineChartBase<*>) :
+        ValueFormatter() {
+
+    override fun getFormattedValue(value: Float): String {
+        val hours = (value / 60).toInt()
+        val restmin = (value - hours*60).toInt()
+
+        if (hours >= 1) {
+            return if (restmin > 0) "$hours hr $restmin min" else "$hours hr"
+        }
+        return "${value.toInt()} min"
+
+    }
 }
